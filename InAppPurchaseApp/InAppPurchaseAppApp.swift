e9ce2b91f9765e87e4ee06f21e0025a707d0fa98
Ak <akbarrkhan@outlook.com>
2023-03-15 16:26:28 +0500

    Initial Commit

---
+++ InAppPurchaseApp/InAppPurchaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InAppPurchaseAppApp.swift
+//  InAppPurchaseApp
+//
+//  Created by Mac on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct InAppPurchaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
